97ede3ebda81cc9755fc21d3be0a2bc1b16c543f
joaocosta21 <joaocostared@gmail.com>
2023-07-31 17:22:37 +0100

    scipt try to use google directions api

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let map;
 
-function initMap(){
+function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
         center: { lat: 38.7223, lng: -9.1393 },
         zoom: 8,
